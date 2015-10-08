7e17efbeead68cd454693971c1c5b8ede520e7ae
serekmt <serkemt@wp.pl>
2015-10-08 23:29:08 +0200

    Slowly move to angular

---
+++ WithAngularAndAsp5/src/WithAngularAndAsp5/wwwroot/app.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', []);
+
+
+    var chatController = function ($scope) {
+
+        var chat = $.connection.chatHub;
+        chat.client.reciveMessage = function (user) {
+            var encodedName = $('<div />').text(user.name).html();
+            var encodedMsg = $('<div />').text(user.message).html();
+            $('#allMessages').append('<li><strong>' + encodedName
+                + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+        };
+
+        $('#displayname').val(prompt('Enter your name:', ''));
+
+        $('#message').focus();
+
+        $.connection.hub.start().done(function () {});
+
+        $('#sendmessage').click(function () {
+            chat.server.send($('#displayname').val(), $('#message').val());
+            $('#message').val('').focus();
+        });
+    };
+
+    app.controller('chatController', ['$scope', chatController]);
+})();
