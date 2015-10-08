4fdaf06380f2394f52a563a46584d55b27d566ec
serekmt <serkemt@wp.pl>
2015-10-08 22:18:30 +0200

    SignalR chat on Asp 5

---
+++ SignalRvNextChat/src/SignalRvNextChat/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRvNextChat
+{
+    class User
+    {
+        public string name { get; private set; }
+        public string message { get; private set; }
+        public User(string name, string message)
+        {
+            this.name = name;
+            this.message = message;
+        }
+    }
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.reciveMessage(new User (name, message));
+        }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+    }
+}
