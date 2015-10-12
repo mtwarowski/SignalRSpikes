a99763b176b6307b2752d6172bb73f4bc9ad6ce4
serekmt <serkemt@wp.pl>
2015-10-12 23:03:04 +0200

    Move ChatHub to service in angular

--- WithAngularAndAsp5/src/WithAngularAndAsp5/wwwroot/app.js
+++ WithAngularAndAsp5/src/WithAngularAndAsp5/wwwroot/app.js
@@ -3,30 +3,31 @@
 
     var app = angular.module('app', []);
 
+    $.connection.hub.start().done(function () { });
 
-    var chatController = function ($scope) {
+    app.value('chatHub', $.connection.chatHub);
+
+    var chatController = function ($scope, chatHub) {
         $scope.posts = [];
         $scope.message = '';
 
-        var chat = $.connection.chatHub;
-        chat.client.reciveMessage = function (post) {
+        chatHub.client.reciveMessage = function (post) {
             
             $scope.$apply(function() {
                 $scope.posts.push(post);
             });
         };
 
-        $.connection.hub.start().done(function () {});
 
         $scope.displayname = prompt('Enter your name:', '');
 
 
         var sendMessage = function () {
-            chat.server.send($scope.displayname, $scope.message);
+            chatHub.server.send($scope.displayname, $scope.message);
         };
 
         $scope.sendMessage = sendMessage;
     };
 
-    app.controller('chatController', ['$scope', chatController]);
+    app.controller('chatController', ['$scope', 'chatHub', chatController]);
 })();
